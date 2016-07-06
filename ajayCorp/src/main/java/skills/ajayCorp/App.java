55ce43a83c704317961ab4e070858892efb801ef
Ajay <ajayace88@gmail.com>
2016-07-07 02:51:21 +0530

    Initial Commit

---
+++ ajayCorp/src/main/java/skills/ajayCorp/App.java
@@ -0,0 +1,13 @@
+package skills.ajayCorp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
